cd7b599af2ac0ba0a8562b351603e20aeb76e21e
Eddy Guo <k1219851@gmail.com>
2023-01-01 22:26:08 -0600

    1/1/23 progress

--- pages/student.js
+++ pages/student.js
@@ -29,26 +29,27 @@ export default function FirstPost() {
 
   return (
     
- 
     <div class="center" style={{borderWidth: '1px', borderStyle: 'solid' ,position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: '400px', borderRadius: '10px', boxShadow: '10px 10px 15px rgba(0,0,0,0.05)'}}>
-     { isSuccess && success() }
-      <h1 style={{ textAlign: 'center', fontSize: '2rem', margin: '1rem 0' }}>Login</h1>
+     { isSuccess ? success() : null }
+     <h1 style={{ padding: '10px', textAlign: 'center', fontSize: '2.5rem', margin: '1rem 0' }}>
+     <span style={{ color: '#0070f3' }}>Student</span> Login
+   </h1>
       <form style={{ margin: '1rem 0' }}>
-        <label htmlFor="username" style={{display: 'block', margin: '0.5rem 0', fontSize: '1.2rem' }}>Username:</label>
+        <label placeholder="Username" htmlFor="username" style={{display: 'block', margin: '0.5rem 0', padding: '10px', fontSize: '1.4rem' }}>Username:</label>
         <br />
         <input type="text" id="username" style={{ display: 'block', width: '50%', margin: ' 0 auto', width: '80%', padding: '0.5rem', fontSize: '1.2rem', border: '1px solid #ccc', borderRadius: '5px' }}/>
         <br />
-        <label htmlFor="password" style={{ display: 'block', margin: '0.5rem 0', padding: '10px', fontSize: '1.2rem' }}> Password:</label>
+        <label htmlFor="password" style={{ display: 'block', margin: '0.5rem 0',  padding: '10px', fontSize: '1.4rem' }}> Password:</label>
         <br />
         <input type="password" id="password" style={{ display: 'block', width: '50%', margin: '0 auto', width: '80%', padding: '0.5rem', fontSize: '1.2rem', border: '1px solid #ccc', borderRadius: '5px' }}/>
         <br />
         <br />
-        <button type="submit" onClick={login} style={{ display: 'block', margin: '0 auto', width: '80%', padding: '0.5rem', fontSize: '1.2rem', border: '1px solid #ccc', borderRadius: '5px' }}>
+        <button type="submit" onClick={login} style={{ background: '#0070f3', display: 'block', margin: '0 auto', width: '80%', padding: '0.5rem', fontSize: '1.2rem', border: '1px solid #ccc', borderRadius: '5px' }}>
           Login
         </button>
       </form>
+      { !isSuccess ? incorrect() : null }
     </div>
-   
   );
 
 }
@@ -58,5 +59,11 @@ function success() {
   window.location.href = 'studentInterface/';
   return null;
 }
+
+//how do i make the error message not show up before i hit the log in button :(
+  function incorrect() {
+    return <p style={{  display: 'block', width: '50%', margin: '0 auto' }}>It appears your username or password is incorrect</p>
+  }
+
   
 
